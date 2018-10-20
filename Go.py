5b2ba12de7e516c9f233ea33d5099ba40e120fd2
jalel <jalel.zghonda@epfl.ch>
2018-10-20 02:30:12 +0100

    modified implementation : games.py tic tac toe inspired

--- Go.py
+++ Go.py
@@ -1,6 +1,7 @@
 print("hello world")
 
 from array import *
+from collections import namedtuple
 
 fileName = 'board.txt'
 
@@ -8,31 +9,30 @@ fileName = 'board.txt'
 class Game:
     Players = BLACK, WHITE = (1, 2)
     Player: int
+    Action = namedtuple('Action', 'x, y')
+    State = namedtuple('State', 'board, player, moves, utility')
 
-    Action = (Players, int, int)
-    State = tuple(list,Player,)
+    def to_move(self, state):
+        return state.player
 
+    def terminal_test(self, state):
+        return state.utility != 0 or len(state.moves) == 0
 
-    def to_move(self, state: State):
-        return state
+    def utility(self, state, player):
+        return state.utility if player == state.player else -state.utility
 
-    def terminal_test(self, state:State):
-        board = state[0]
+    def actions(self, state):
+        return state.moves
 
-        for i in board :
-            for j in board[i] :
-                player = board[i][j]
-                if(player != 0 &&   )
-        return
-
-    def utility(self, s: State, player):
-        return
-
-    def actions(self, s: State):
-        return
-
-    def result(self, s: State, a: Action):
-        return
+    def result(self, state, action):
+        if action not in state.moves:
+            return state
+        state.board[action.x - 1][action.y - 1] = state.player
+        board = state.board.copy()
+        board[action.x - 1][action.y - 1] = state.player
+        moves = list(state.moves)
+        moves.remove(action)
+        return self.State(board, '1' if state.player == '0' else '0', moves, 0)
 
     def load_board(self, file=open(fileName, 'r')):
         first_line = file.readline()
@@ -40,11 +40,17 @@ class Game:
         board_size = int(first_line[0])
         current_player = first_line[2]
         board = list(map(lambda s: list(filter(lambda c: c != '\n', list(s))), file.readlines()))
+        moves = list()
+        for i in range(0, len(board)):
+            for j in range(0, len(board[i])):
+                if board[i][j] == '0':
+                    moves.append((i + 1, j + 1))
         file.close()
-        return board, current_player, board_size
-
-    board_size = load_board()[2]
+        return board, current_player, moves, 0
+    def compute_utility
 
 
 game = Game()
-game.load_board()
+state = game.load_board()
+print(state[0])
+print(state)

48acd2ac67f26d9012252d7d64865dfc68c02b29
jalel <jalel.zghonda@epfl.ch>
2018-10-29 01:11:10 +0000

    removed state from a few functions arguments and replaced it by the board to make it simpler

--- Go.py
+++ Go.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 
 fileName = 'board.txt'
 
+
 class Game:
     Action = namedtuple('Action', 'i, j, player')
     State = namedtuple('State', 'board, player, moves, utility')
@@ -33,9 +34,12 @@ class Game:
         moves = list(state.moves)
         moves.remove(action)
         moves = list(
-            filter(lambda m: not (self.is_suicide(state, self.Action(m[0][1], m[0][1], m[1]))),
+            filter(lambda m: not (self.is_suicide(state.board, self.Action(m[0][1], m[0][1], m[1]))),
                    moves))
-        return self.State(board, '1' if state.player == '2' else '0', moves, self.compute_utility(state, action))
+        return self.State(board, self.next_player(action), moves, self.compute_utility(state, action))
+
+    def next_player(self, player):
+        return self.BLACK if player == self.WHITE else self.WHITE
 
     def load_board(self, file=open(fileName, 'r')):
         first_line = file.readline()
@@ -53,42 +57,40 @@ class Game:
                          ]))
         print(moves)
         file.close()
-        state = self.State(board, current_player, moves, 0)
-        return self.State(state.board, state.player,
+        return self.State(board, current_player,
                           list(
-                              filter(lambda m: not (self.is_suicide(state, self.Action(m[0][1], m[0][1], m[1]))),
-                                     moves)),
-                          state.utility)
+                              filter(lambda m: not (self.is_suicide(board, self.Action(m[0][1], m[0][1], m[1]))),
+                                     moves)), 0)
 
-    def neighbors(self, state, i, j):
+    def neighbors(self, board, i, j):
         surrounding = ((i, j - 1), (i + 1, j), (i, j + 1), (i - 1, j))
-        return map(lambda pos: (state.board[pos[0]][pos[1]], (pos[0], pos[1])),
-                   filter(lambda coord: self.check_edge_overflow(coord[0], coord[1], self.size), surrounding))
+        return list(map(lambda pos: (board[pos[0]][pos[1]], (pos[0], pos[1])),
+                   filter(lambda coord: self.check_edge_overflow(coord[0], coord[1], self.size), surrounding)))
 
-    def get_liberties(self, state, i, j):
+    def get_liberties(self, board, i, j):
 
-        def get_liberties_helper(self, state, i, j, traversed):
+        def get_liberties_helper(self, board, i, j, traversed):
             """assert (self.check_edge_overflow(self, i, j, self.size))"""
-            location = state.board[i][j]
+            location = board[i][j]
             if location == self.EMPTY:
                 return set([(i, j)])
             else:
                 surroundings = [
                     (loc, (x, y))
-                    for loc, (x, y) in self.neighbors(state, i, j)
+                    for loc, (x, y) in self.neighbors(board, i, j)
                     if (loc == location or loc == self.EMPTY) and (x, y) not in traversed
                 ]
                 traversed.add((i, j))
 
                 if surroundings:
                     return set.union(*[
-                        get_liberties_helper(self, state, a, b, traversed)
+                        get_liberties_helper(self, board, a, b, traversed)
                         for _, (a, b) in surroundings
                     ])
                 else:
                     return set()
 
-        return get_liberties_helper(self, state, i, j, set())
+        return get_liberties_helper(self, board, i, j, set())
 
     def shift_to_game(self, value):
         return value + 1
@@ -99,16 +101,16 @@ class Game:
     def check_edge_overflow(self, i, j, edge):
         return i in range(0, edge) and j in range(0, edge)
 
-    def is_capture(self, state, i, j):
-        return self.get_liberties(state, i, j)
+    def is_in_captured_group(self, board, i, j):
+        return len(self.get_liberties(board, i, j)) == 0
 
-    def is_suicide(self, state, action):
+    def is_suicide(self, board, action):
         # list are not immutable, solution has to be improved for esthetic reasons
-        temp_board = state.board.copy()
+        temp_board = board.copy()
         i, j, p = (action.i, action.j, action.player)
         old = temp_board[i][j]
         temp_board[i][j] = p
-        num_liberties = len(self.get_liberties(self.State(temp_board, p, state.moves, state.utility), i, j))
+        num_liberties = len(self.get_liberties(temp_board, i, j))
         temp_board[i][j] = old
         return num_liberties == 0
 
@@ -117,7 +119,7 @@ class Game:
             (i, j)
             for i in range(0, self.size)
             for j in range(0, self.size)
-            if (state.board[i][j] != self.EMPTY and len(self.get_liberties(state, i, j)) == 0)
+            if (state.board[i][j] != self.EMPTY and self.is_in_captured_group(state, i, j))
         ]
         assert (len(list) <= 1)
         if len(list) == 0:
@@ -130,5 +132,5 @@ class Game:
 game = Game()
 state = game.load_board()
 print("board is ", state.board)
-print(game.get_liberties(state, 2, 1))
+print(game.get_liberties(state.board, 2, 1))
 print(state.moves)

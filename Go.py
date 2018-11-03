7eeea026144841f4ffe0502be39c0d47ea15d2f4
jalel <jalel.zghonda@epfl.ch>
2018-11-03 23:13:11 +0000

    corrected a lot of mistakes, now ready for testing eval function needed

--- Go.py
+++ Go.py
@@ -1,111 +1,116 @@
 from array import *
 from collections import namedtuple
+from copy import *
+from itertools import *
 
 fileName = 'board.txt'
 infinity = float('inf')
 
 
 class Game:
-    Action = namedtuple('Action', 'i, j, player')
-    State = namedtuple('State', 'board, player, moves, utility')
+    # Action = tuple('i, j, player')
+    state = namedtuple('state', 'board, player, moves')
 
     def __init__(self):
         self.size = 0
-        self.EMPTY = '0'
-        self.BLACK = '1'
-        self.WHITE = '2'
+        self.EMPTY = 0
+        self.BLACK = 1
+        self.WHITE = 2
 
     def to_move(self, state):
         return state.player
 
     def terminal_test(self, state):
-        return state.utility != 0 or len(state.moves) == 0
+        ''' Return True if state is terminal '''
+        if self.check_if_draw(state): return True
+        # One piece has no liberties
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                if self.is_in_captured_group(state.board, i, j):
+                    return True
+        return False
 
     def utility(self, state, player):
-        return state.utility if player == state.player else -state.utility
+        if self.terminal_test(state):
+            # Check if draw
+            if self.check_if_draw(state): return 0
+            for i in range(0, self.size):
+                for j in range(0, self.size):
+                    if self.is_in_captured_group(state.board, i, j):
+                        if state.board[i][j] == player: return -1
+                        return 1
+        return 99
+
+    def check_if_draw(self, state):
+        '''' Returns True or False whether it's a draw '''
+
+        return len(self.generate_moves(state.board)) == 0
+        #
+        # for certain_player in range(1, 3):
+        #   count_moves = 0
+        #  for i in range(0, len(state.moves)):
+        #     if state.moves[i][0] == certain_player: count_moves += 1
+        # if count_moves == 0: return True
+        # return False
 
     def actions(self, state):
-        return state.moves
+        return list(filter(lambda m: m[0] == state.player, state.moves))
 
     def result(self, state, action):
-        if action not in state.moves:
-            return state
-        board = state.board.copy()
-        board[action.i][action.j] = action.player
-        moves = list(state.moves)
-        moves.remove(action)
-        moves = list(
-            filter(lambda m: not (self.is_suicide(state.board, self.Action(m[0][1], m[0][1], m[1]))),
-                   moves))
-        return self.State(board, self.next_player(action), moves, self.compute_utility(state, action))
+        board = self.apply_action(state, action)
 
+        if action in state.moves:
+            print("yes")
+            print(action)
+            state.moves.remove(action)
+
+        player = action[0]
+        moves = self.remove_suicide(board, state.moves, player)
+        return self.state(board, self.next_player(player), moves)
+
+    def apply_action(self, board, action):
+        board = deepcopy(board)
+        (p, i, j) = (action[0], self.shift_to_computer(action[1]), self.shift_to_computer(action[2]))
+        board[i][j] = p
+        return board
+
+    def remove_suicide(self, board, moves, player):
+        # <0 because result flips the player turn
+        return list(
+            filter(lambda m: not (self.is_suicide(board, m)
+                                  and not (
+                            self.utility(self.state(self.apply_action(board, m), player, moves), player) == 1)),
+                   moves))
+
+    # def load_board(self, fileName):
+    # file = open(fileName, 'r')
     def load_board(self, file=open(fileName, 'r')):
         first_line = file.readline()
         assert (len(first_line) >= 3)
         self.size = int(first_line[0])
-        current_player = first_line[2]
-        board = list(map(lambda s: list(filter(lambda c: c != '\n', list(s))), file.readlines()))
-        moves = list(map(lambda m:
-                         ((m[0][0], m[0][1]), str(m[1])), [
-                             ((i, j), p)
-                             for p in range(1, 3)
-                             for i in range(0, self.size)
-                             for j in range(0, self.size)
-                             if (board[i][j] == self.EMPTY)
-                         ]))
+        current_player = int(first_line[2])
+        board = list()
+        for line in file.readlines():
+            board.append(list(map(lambda char: int(char), list(filter(lambda c: c != '\n', list(line))))))
+
+        moves = self.generate_moves(board)
+        print(moves)
+        moves = self.remove_suicide(deepcopy(board), moves, current_player)
         print(moves)
         file.close()
-        return self.State(board, current_player,
-                          list(
-                              filter(lambda m: not (self.is_suicide(board, self.Action(m[0][1], m[0][1], m[1]))),
-                                     moves)), 0)
+        # Put terminal_test in print
+        # print(self.terminal_test(self.result(self, state, (1, 1, '1'))))
 
-    def alphabeta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None):
-        """Search game to determine best action; use alpha-beta pruning.
-        This version cuts off search and uses an evaluation function."""
+        return self.state(board, current_player, moves)
 
-        player = game.to_move(state)
-
-        # Functions used by alphabeta
-        def max_value(state, alpha, beta, depth):
-            if cutoff_test(state, depth):
-                return eval_fn(state)
-            v = -infinity
-            for a in game.actions(state):
-                v = max(v, min_value(game.result(state, a),
-                                     alpha, beta, depth + 1))
-                if v >= beta:
-                    return v
-                alpha = max(alpha, v)
-            return v
-
-        def min_value(state, alpha, beta, depth):
-            if cutoff_test(state, depth):
-                return eval_fn(state)
-            v = infinity
-            for a in game.actions(state):
-                v = min(v, max_value(game.result(state, a),
-                                     alpha, beta, depth + 1))
-                if v <= alpha:
-                    return v
-                beta = min(beta, v)
-            return v
-
-        # Body of alphabeta_cutoff_search starts here:
-        # The default test cuts off at depth d or at a terminal state
-        cutoff_test = (cutoff_test or
-                       (lambda state, depth: depth > d or
-                                             game.terminal_test(state)))
-        eval_fn = eval_fn or (lambda state: game.utility(state, player))
-        best_score = -infinity
-        beta = infinity
-        best_action = None
-        for a in game.actions(state):
-            v = min_value(game.result(state, a), best_score, beta, 1)
-            if v > best_score:
-                best_score = v
-                best_action = a
-        return best_action
+    def generate_moves(self, board):
+        return [
+            (p, self.shift_to_game(i), self.shift_to_game(j))
+            for p in range(1, 3)
+            for i in range(0, self.size)
+            for j in range(0, self.size)
+            if (board[i][j] == self.EMPTY)
+        ]
 
     def neighbors(self, board, i, j):
         surrounding = ((i, j - 1), (i + 1, j), (i, j + 1), (i - 1, j))
@@ -116,12 +121,11 @@ class Game:
         return self.BLACK if player == self.WHITE else self.WHITE
 
     def get_liberties(self, board, i, j):
-
         def get_liberties_helper(self, board, i, j, traversed):
             """assert (self.check_edge_overflow(self, i, j, self.size))"""
             location = board[i][j]
             if location == self.EMPTY:
-                return set([(i, j)])
+                return {(i, j)}
             else:
                 surroundings = [
                     (loc, (a, b))
@@ -154,33 +158,86 @@ class Game:
 
     def is_suicide(self, board, action):
         # list are not immutable, solution has to be improved for esthetic reasons
-        temp_board = board.copy()
-        i, j, p = (action.i, action.j, action.player)
-        old = temp_board[i][j]
+        temp_board = deepcopy(board)
+        p, i, j = (action[0], self.shift_to_computer(action[1]), self.shift_to_computer(action[2]))
         temp_board[i][j] = p
-        num_liberties = len(self.get_liberties(temp_board, i, j))
-        temp_board[i][j] = old
-        return num_liberties == 0
+        return self.is_in_captured_group(temp_board, i, j)
 
-    def compute_utility(self, state, action):
-        list = [
-            (i, j)
-            for i in range(0, self.size)
-            for j in range(0, self.size)
-            if (state.board[i][j] != self.EMPTY and self.is_in_captured_group(state, i, j))
-        ]
-        assert (len(list) <= 1)
-        if len(list) == 0:
-            return 0
-        else:
-            (a, b) = list.pop()
-            return -1 if state.player == state.board[a][b] else 1
+    def evalfn(self, state, player):
+        my_conglomerates = []
+        enemy_conglomerates = []
+        board_pos = [self.size + 1][self.size + 1]
+        # Not sure about max
+        # my_pieces = list(map(lambda pos: state.board[pos[0]][pos[1]] == player, board_pos))
+        my_pieces = [[(i, j), "i"]
+                     for i in range(0, self.size)
+                     for j in range(0, self.size)
+                     if (state.board[i][j] == player)
+                     ]
+        # enemy_pieces = list(map(lambda pos: state.board[pos[0]][pos[1]] == str((int(player)%2)+1), board_pos))
+        enemy_pieces = [[(i, j), "i"]
+                        for i in range(0, self.size)
+                        for j in range(0, self.size)
+                        if (state.board[i][j] == str((int(player) % 2) + 1))
+                        ]
+
+        for i in range(0, len(my_pieces)):
+            if (my_pieces[i][1] == "i"):
+                neighbours = game.neighbors(state.board, my_pieces[i][0][0], my_pieces[i][0][1])
+                for j in range(0, len(neighbours)):
+                    for z in range(0, len(my_conglomerates)):
+                        if (neighbours[j][1] in my_conglomerates[z]):
+                            my_pieces[i][1] = z
+                            my_conglomerates[z].append(my_pieces[i][0])
+            if (my_pieces[i][1] == "i"):
+                my_pieces[i][1] = len(my_conglomerates)
+                my_conglomerates.append([my_pieces[i][0]])
+
+        for i in range(0, len(enemy_pieces)):
+            if (enemy_pieces[i][1] == "i"):
+                neighbours = game.neighbors(state.board, enemy_pieces[i][0][0], enemy_pieces[i][0][1])
+                for j in range(0, len(neighbours)):
+                    for z in range(0, len(enemy_conglomerates)):
+                        if (neighbours[j][1] in enemy_conglomerates[z]):
+                            enemy_pieces[i][1] = z
+                            enemy_conglomerates[z].append(enemy_pieces[i][0])
+            if (enemy_pieces[i][1] == "i"):
+                enemy_pieces[i][1] = len(enemy_conglomerates)
+                enemy_conglomerates.append([enemy_pieces[i][0]])
+
+        my_liberties = []
+        for i in range(0, 3):
+            count = 0
+            for j in range(0, len(my_conglomerates)):
+                if (len(game.get_liberties(state.board, my_conglomerates[j][0][0],
+                                           my_conglomerates[j][0][1])) == i + 1):
+                    count = count + 1
+            my_liberties.append(count)
+        enemy_liberties = []
+        for i in range(0, 3):
+            count = 0
+            for j in range(0, len(enemy_conglomerates)):
+                if (len(game.get_liberties(state.board, enemy_conglomerates[j][0][0],
+                                           enemy_conglomerates[j][0][1])) == i + 1):
+                    count += 1
+            enemy_liberties.append(count)
+        my_liberties[2] = my_liberties[2] / 2
+        enemy_liberties[2] = enemy_liberties[2] / 2
+        print(my_liberties)
+        print(enemy_liberties)
+        Eval = 0
+        for i in range(0, 3):
+            Eval += my_liberties[i] - enemy_liberties[i]
+        if Eval < 0:
+            return Eval - 2
+        if Eval >= 0:
+            return Eval + 2
 
 
 game = Game()
 state = game.load_board()
+
 print("board is ", state.board)
-print(game.get_liberties(state.board, 1, 2))
-print("board is ", state.board)
-print(game.neighbors(state.board, 1, 2))
-print(state.moves)
+print(state)
+board = game.apply_action(state.board, (1, 2, 4))
+print(board)
